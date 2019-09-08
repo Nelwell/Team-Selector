8c22990c7398ba8f91a7c26111627214f3788fe9
Nick <cloud@gmail.com>
2019-09-07 20:12:28 -0500

    Added animation to results upon running. Extracted separate method for selecting team. Cleaned up code.

--- app/src/main/java/com/example/teamselector/MainActivity.java
+++ app/src/main/java/com/example/teamselector/MainActivity.java
@@ -15,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText teamOne;
     private EditText teamTwo;
-    //    private TextView result = findViewById(R.id.result_here);
+    private TextView result;
+    private Button teamSelectionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
         // Stores string resource in variable
         teamOne = findViewById(R.id.team_one);
         teamTwo = findViewById(R.id.team_two);
-        Button teamSelectionButton = findViewById(R.id.select_team);
-        final TextView result = findViewById(R.id.result_here);
+        teamSelectionButton = findViewById(R.id.select_team);
+        result = findViewById(R.id.result_here);
 
         teamSelectionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,36 +44,22 @@ public class MainActivity extends AppCompatActivity {
                     result.setText("");
                     return;
                 }
-                // Pick a random boolean
-                boolean teamOne = new Random().nextBoolean();
-
-                if (teamOne) {
-                    result.setText(firstTeam);
-                } else {
-                    result.setText(secondTeam);
-                }
+                chooseRandomTeam(firstTeam, secondTeam);
             }
         });
     }
 
-    // Removed, kept crashing app
+    private void chooseRandomTeam(String firstTeam, String secondTeam) {
+        // Pick a random boolean
+        boolean teamOne = new Random().nextBoolean();
 
-//    private void chooseRandomTeam() {
-//
-//        teamOne = findViewById(R.id.team_one);
-//        teamTwo = findViewById(R.id.team_two);
-//
-//        // Reads text in EditTexts
-//        String firstTeam = teamOne.getText().toString();
-//        String secondTeam = teamTwo.getText().toString();
-//
-//        // Pick a random boolean
-//        boolean teamOne = new Random().nextBoolean();
-//
-//        if (teamOne) {
-//            result.setText(firstTeam);
-//        } else {
-//            result.setText(secondTeam);
-//        }
-//    }
+        if (teamOne) {
+            result.setText(firstTeam);
+        } else {
+            result.setText(secondTeam);
+        }
+        // Added "refresh" animation to make it clear when Team Selection Button is pressed, even with same result
+        result.setAlpha(0);
+        result.animate().alpha(1);
+    }
 }
